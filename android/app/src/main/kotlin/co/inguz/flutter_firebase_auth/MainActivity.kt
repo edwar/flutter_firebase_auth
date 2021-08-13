ee74550b24efcf7c0642dd59a2462f88983cd7fa
Edwar Amaya <edwaramayadiaz@gmail.com>
2021-08-13 00:28:52 -0500

    Config facebook auth in android

--- android/app/src/main/kotlin/co/inguz/flutter_firebase_auth/MainActivity.kt
+++ android/app/src/main/kotlin/co/inguz/flutter_firebase_auth/MainActivity.kt
@@ -1,6 +1,30 @@
 package co.inguz.flutter_firebase_auth
 
+//import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+//import java.security.NoSuchAlgorithmException
+
+//import javax.naming.NameNotFoundException
+
+//import java.security.MessageDigest
+
+
+
 
 class MainActivity: FlutterActivity() {
+    //override fun onCreate(savedIntanceState: Bundle? {
+    //    super.onCreate(savedIntanceState)
+    //    try {
+    //        val info: PackageInfo = getPackageManager().getPackageInfo(
+    //                "co.inguz.flutter_firebase_auth",  //Insert your own package name.
+    //                PackageManager.GET_SIGNATURES)
+    //        for (signature in info.signatures) {
+    //            val md: MessageDigest = MessageDigest.getInstance("SHA")
+    //            md.update(signature.toByteArray())
+    //            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+    //        }
+    //    } catch (e: NameNotFoundException) {
+    //    } catch (e: NoSuchAlgorithmException) {
+    //    }
+    //}
 }
